549b0b2616d5480cce637bb5d10d8b92544a8b7c
Joonas Ikonen <joonas.ikonen@tuni.fi>
2022-01-03 11:47:09 +0200

    pylint super.py

--- db/super.py
+++ db/super.py
@@ -4,6 +4,7 @@ import sqlite3
 
 
 class SQLTableBase:
+    """The super class for sql tables."""
 
     TABLENAME = 0
     KEY = 1
@@ -26,15 +27,15 @@ class SQLTableBase:
     def __init__(self, connection):
         self.con: sqlite3.Connection = connection
         self.columns_table_keys = [
-            "columns_id", 
-            "tablename",  
-            "key",        
-            "label",      
-            "type",       
-            "col_idx",    
-            "col_order",  
-            "width",      
-            "ro",         
+            "columns_id",
+            "tablename",
+            "key",
+            "label",
+            "type",
+            "col_idx",
+            "col_order",
+            "width",
+            "ro",
             "visible"
         ]
         self.variables_table_keys = [
@@ -54,14 +55,14 @@ class SQLTableBase:
 
     def create(self):
         """Create the table and it's indexes."""
-        
+
         # print("Existing columns entries: ")
         # coldata = self.con.execute(
         #     "SELECT * FROM columns"
         # )
         # for row in coldata:
         #     print(row)
-        
+
         try:
             with self.con:
                 # Create table and indexes.
@@ -71,8 +72,8 @@ class SQLTableBase:
 
                 try:
                     self.create_undo_triggers()
-                except sqlite3.OperationalError as e:
-                    print("Could not create undo triggers: {}".format(e))
+                except sqlite3.OperationalError as err:
+                    print(f"Could not create undo triggers: {err}")
 
                 # Insert default columns values if required.
                 count = self.con.execute(
@@ -80,22 +81,21 @@ class SQLTableBase:
                     (self.name,)
                 )
                 if count != len(self.default_columns):
-                    for n, col in enumerate(self.default_columns):
-                        ro = 1 if col[self.KEY] in self.read_only else 0
+                    for i, col in enumerate(self.default_columns):
+                        read_only = 1 if col[self.KEY] in self.read_only else 0
                         try:
+                            keys = ",".join(self.columns_table_keys[1:])
                             self.con.execute(
-                                "INSERT INTO columns ({k}) VALUES (?,?,?,?,?,?,?,?,?)".format(
-                                    k=",".join(self.columns_table_keys[1:])
-                                ),
-                                list(col) + [n, n, 60, ro, 1]
+                                f"INSERT INTO columns ({keys}) VALUES (?,?,?,?,?,?,?,?,?)",
+                                list(col) + [i, i, 60, read_only, 1]
                             )
                         except sqlite3.IntegrityError:
                             pass
 
-        except sqlite3.OperationalError as e:
+        except sqlite3.OperationalError as err:
             if SQLTableBase.print_errors:
-                print("\nsqlite3.OperationalError: {}".format(e))
-                print("Could not create table: {}".format(self.name))
+                print(f"\nsqlite3.OperationalError: {err}")
+                print(f"Could not create table: {self.name}")
 
     def execute_dml(self, sql: str, values: list=None, many: bool=False, rowid: bool=False) -> bool:
         """Run execute on a data manipulation language string.
@@ -134,12 +134,12 @@ class SQLTableBase:
                     else:
                         cur = self.con.execute(sql, values)
 
-        except sqlite3.Error as e:
+        except sqlite3.Error as err:
             if SQLTableBase.print_errors:
-                print("\n{}: {}".format(type(e), e.args[0]))
+                print(f"\n{type(err)}: {err.args[0]}")
                 print("In SQLTableBase.execute_dml")
-                print("Error with sql: {}".format(sql))
-                print("using values: {}".format(values))
+                print(f"Error with sql: {sql}")
+                print(f"using values: {values}")
             return False
 
         if rowid:
@@ -149,7 +149,7 @@ class SQLTableBase:
 
     def execute_dql(self, sql: str, values: list=None, cursor: bool=False) -> list:
         """Execute a Data Query Language command.
-        
+
         Used for SQL SELECT statements.
 
         Parameters
@@ -175,12 +175,12 @@ class SQLTableBase:
                 else:
                     cur = self.con.execute(sql, values)
 
-        except sqlite3.Error as e:
+        except sqlite3.Error as err:
             if SQLTableBase.print_errors:
-                print("\n{}: {}".format(type(e), e))
+                print(f"\n{type(err)}: {err}")
                 print("In SQLTableBase.execute_dql")
-                print("Error with sql: {}".format(sql))
-                print("using values: {}".format(values))
+                print(f"Error with sql: {sql}")
+                print(f"using values: {values}")
             return None
 
         if cursor:
@@ -215,19 +215,17 @@ class SQLTableBase:
             rep_str = " OR REPLACE "
         else:
             rep_str = " "
-        sql = "INSERT{u}INTO {t}({k}) VALUES ({b})".format(
-            u=rep_str, t=self.name, k=keys_str, b=binds
-        )
+        sql = f"INSERT{rep_str}INTO {self.name}({keys_str}) VALUES ({binds})"
 
         result = self.execute_dml(sql, values, many, not many)
         return result
 
-    def insert_empty(self, fk: int=None) -> int:
+    def insert_empty(self, foreign_key: int=None) -> int:
         """Insert an empty row.
 
         Parameters
         ----------
-        fk : int
+        foreign_key : int
             A foreign key or None if the table uses no foreign key.
 
         Returns
@@ -237,17 +235,17 @@ class SQLTableBase:
         """
         sql_ins = "INSERT INTO {t}"
         sql_val = "({k}) VALUES (?)"
-        if fk is None:
+        if foreign_key is None:
             sql = sql_ins.format(t=self.name) + " DEFAULT VALUES"
             values = None
         else:
             sql = sql_ins.format(t=self.name) + sql_val.format(k=self.foreign_key)
-            values = (fk,)
+            values = (foreign_key,)
         rowid = self.execute_dml(sql, values, rowid=True)
 
         return rowid
 
-    def update(self, pk: int, col: int, value) -> bool:
+    def update(self, pk_value: int, col: int, value) -> bool:
         """Update a single value in the table.
 
         Parameters
@@ -265,21 +263,18 @@ class SQLTableBase:
             True on success. False on Error.
         """
         key = self.col2key(col)
-        sql = "UPDATE {t} SET {k}=(?) WHERE {pk}=(?)".format(
-            t=self.name,
-            k=key,
-            pk=self.primary_key
-        )
-        values = (value, pk)
+        sql = f"UPDATE {self.name} SET {key}=(?) WHERE {self.primary_key}=(?)"
+        values = (value, pk_value)
         result = self.execute_dml(sql, values)
         return result
 
-    def delete(self, pk: int) -> bool:
-        sql = "DELETE FROM {t} WHERE {pk}=(?)".format(t=self.name, pk=self.primary_key)
-        values = (pk,)
+    def delete(self, pk_value: int) -> bool:
+        """Delete the row with the matching primary key."""
+        sql = f"DELETE FROM {self.name} WHERE {self.primary_key}=(?)"
+        values = (pk_value,)
         return self.execute_dml(sql, values)
 
-    def undo(self, fk: int=None) -> bool:
+    def undo(self, foreign_key: int=None) -> bool:
         """Undo the last action.
 
         Get last statement from undostack. Execute the statement.
@@ -289,7 +284,7 @@ class SQLTableBase:
 
         Parameters
         ----------
-        fk : int, optional
+        foreign_key : int, optional
             Key to the specific undostack, by default None
 
         Returns
@@ -297,14 +292,15 @@ class SQLTableBase:
         bool
             True on success.
         """
-        self.undo_step(fk, True)
+        self.undo_step(foreign_key, True)
         return True
 
-    def redo(self, fk: int=None) -> bool:
-        self.undo_step(fk, False)
+    def redo(self, foreign_key: int=None) -> bool:
+        """Redo the last undo."""
+        self.undo_step(foreign_key, False)
         return True
 
-    def select(self, fk: int=None, filt: dict=None, count: bool=False) -> list:
+    def select(self, foreign_key: int=None, filt: dict=None, count: bool=False) -> list:
         """Get list of rows from the table.
 
         Filters results by using foreign key or filter dictionary.
@@ -312,7 +308,7 @@ class SQLTableBase:
 
         Parameters
         ----------
-        fk : int, optional
+        foreign_key : int, optional
             Foreign key used for filtering the results, by default None
         filt : dict, optional
             A dictionary as a filter in format {key: [operator, value]}, by default None
@@ -331,22 +327,22 @@ class SQLTableBase:
         table_alias = self.get_table_alias() + "."
 
         # Add the foreign key to filter for parsing.
-        if fk is not None and self.foreign_key is not None:
+        if foreign_key is not None and self.foreign_key is not None:
             fk_idx = keys.index(self.foreign_key)
             if filt is None:
-                filt = {fk_idx: ["=", fk]}
+                filt = {fk_idx: ["=", foreign_key]}
             else:
-                filt[fk_idx] = ["=", fk]
+                filt[fk_idx] = ["=", foreign_key]
 
         if filt is not None and len(filt) > 0:
             # Parse a WHERE string from filter dictionary.
             where_str = "{t}{k} {op} (?)"
-            for n, (key, value) in enumerate(filt.items()):
+            for i, (key, value) in enumerate(filt.items()):
                 cond += where_str.format(t=table_alias, k=keys[key], op=value[0])
                 values.append(value[1])
-                if n < len(filt) - 1:
+                if i < len(filt) - 1:
                     cond += " AND "
-            sql_con = " WHERE {}".format(cond)
+            sql_con = f" WHERE {cond}"
             sql = sql_sel + sql_con
 
         else:
@@ -354,7 +350,8 @@ class SQLTableBase:
             sql = sql_sel
             values = None
 
-        sql += " ORDER BY {k} {d}".format(k=self.primary_key, d="ASC")
+        direction = "ASC"
+        sql += f" ORDER BY {self.primary_key} {direction}"
         return self.execute_dql(sql, values)
 
     def get_column_setup(self, key: str, col: int=None):
@@ -404,9 +401,9 @@ class SQLTableBase:
         bool
             True on success.
         """
-        sql = """
-            UPDATE columns SET {k}=(?) WHERE tablename=(?) and col_idx=(?)
-        """.format(k=key)
+        sql = f"""
+            UPDATE columns SET {key}=(?) WHERE tablename=(?) and col_idx=(?)
+        """
         values = (value, self.name, col)
         return self.execute_dml(sql, values)
 
@@ -442,7 +439,7 @@ class SQLTableBase:
 
     def get_insert_keys(self, inc_rowid=False):
         """Return a list of keys for INSERT statements.
-        
+
         Override for tables that have columns that do not allow inserts.
         """
         return self.table_keys if inc_rowid else self.table_keys[1:]
@@ -459,8 +456,10 @@ class SQLTableBase:
             Set True to return query for count of entries instead of content.
         """
         if count:
-            return "SELECT COUNT(*) FROM {t}".format(t=self.name)
-        return "SELECT {k} FROM {t}".format(k=",".join(self.table_keys), t=self.name)
+            return f"SELECT COUNT(*) FROM {self.name}"
+
+        keys = ",".join(self.table_keys)
+        return f"SELECT {keys} FROM {self.name}"
 
     def get_table_alias(self) -> str:
         """Return the table alias used in SELECT queries.
@@ -508,16 +507,13 @@ class SQLTableBase:
             ins_keys
         )
         keys = ins_keys
-        value_strings = map(
-            lambda k: "'||quote(old.{key})||'".format(key=k),
-            ins_keys
-        )
+        value_strings = map(lambda k: f"'||quote(old.{k})||'", ins_keys)
         if self.foreign_key is None:
             fk_str = "NULL"
             dfk_str = "NULL"
         else:
-            fk_str = "quote(new.{})".format(self.foreign_key)
-            dfk_str = "quote(old.{})".format(self.foreign_key)
+            fk_str = f"quote(new.{self.foreign_key})"
+            dfk_str = f"quote(old.{self.foreign_key})"
 
         script = """
         CREATE TEMP TRIGGER {t}_it AFTER INSERT ON {t} BEGIN
@@ -579,7 +575,7 @@ class SQLTableBase:
         )
         _undo['freeze'] = -1
 
-    def undo_barrier(self, fk: int):
+    def undo_barrier(self, foreign_key: int):
         """Create an undo barrier."""
         _undo = SQLTableBase._undo
         SQLTableBase.pending = []
@@ -589,49 +585,49 @@ class SQLTableBase:
             end = _undo['freeze']
 
         try:
-            begin = _undo['firstlog'][fk]
+            begin = _undo['firstlog'][foreign_key]
         except KeyError:
-            _undo['firstlog'][fk] = end
-            begin = _undo['firstlog'][fk]
+            _undo['firstlog'][foreign_key] = end
+            begin = _undo['firstlog'][foreign_key]
 
-        self.start_interval(fk)
+        self.start_interval(foreign_key)
 
         # Nothing to undo after last start interval.
-        if begin == _undo['firstlog'][fk]:
+        if begin == _undo['firstlog'][foreign_key]:
             return
 
         if begin > end:
             begin = end
 
         try:
-            self.undostack[fk].append((begin, end))
+            self.undostack[foreign_key].append((begin, end))
         except KeyError:
-            self.undostack[fk] = [(begin, end)]
-        self.redostack[fk] = []
+            self.undostack[foreign_key] = [(begin, end)]
+        self.redostack[foreign_key] = []
 
         # for k, v in self.undostack.items():
         #     # print("\nfk: {}".format(k))
         #     for interval in v:
         #         # print("\tinterval: {}".format(interval))
 
-    def can_undo(self, fk: int) -> bool:
+    def can_undo(self, foreign_key: int) -> bool:
         """Return True if there is an action that can be undone."""
         try:
-            return len(self.undostack[fk]) > 0
+            return len(self.undostack[foreign_key]) > 0
         except KeyError:
             return False
 
-    def can_redo(self, fk: int) -> bool:
+    def can_redo(self, foreign_key: int) -> bool:
         """Return True if there is an action that can be redone."""
         try:
-            return len(self.redostack[fk]) > 0
+            return len(self.redostack[foreign_key]) > 0
         except KeyError:
             return False
 
-    def start_interval(self, fk: int):
+    def start_interval(self, foreign_key: int):
         """Record the starting seq value of the interval."""
         _undo = SQLTableBase._undo
-        _undo['firstlog'][fk] = self.execute_dql(
+        _undo['firstlog'][foreign_key] = self.execute_dql(
             "SELECT coalesce(max(seq),0)+1 FROM undolog")[0][0]
 
     def get_undo_maxseq(self) -> int:
@@ -639,56 +635,56 @@ class SQLTableBase:
         return self.execute_dql("""
             SELECT coalesce(max(seq),0) FROM undolog""", None, True).fetchone()[0]
 
-    def undo_step(self, fk: int, is_undo: bool=True):
+    def undo_step(self, foreign_key: int, is_undo: bool=True):
         """Do undo or redo action.
 
         Parameters
         ----------
-        fk : int, optional
+        foreign_key : int, optional
             Key to the specific undostack, by default None
         is_undo : bool, optional
             True for undo step, False for redo step.
         """
         _undo = SQLTableBase._undo
         if is_undo:
-            (begin, end) = self.undostack[fk].pop()
+            (begin, end) = self.undostack[foreign_key].pop()
         else:
-            (begin, end) = self.redostack[fk].pop()
+            (begin, end) = self.redostack[foreign_key].pop()
 
         result = self.execute_dql("""
             SELECT sql FROM undolog
             WHERE
                 tablename=(?) AND
-                fk=(?) AND
+                foreign_key=(?) AND
                 seq>=(?) AND seq<=(?)
             ORDER BY seq DESC
-        """, (self.name, fk, begin, end))
+        """, (self.name, foreign_key, begin, end))
 
         self.execute_dml("""
             DELETE FROM undolog
             WHERE
                 tablename=(?) AND
-                fk=(?) AND
+                foreign_key=(?) AND
                 seq>=(?) AND seq<=(?)
-        """, (self.name, fk, begin, end))
+        """, (self.name, foreign_key, begin, end))
 
-        self.start_interval(fk)
+        self.start_interval(foreign_key)
 
         for (sql,) in result:
             self.execute_dml(sql)
 
         end = self.get_undo_maxseq()
-        begin = _undo['firstlog'][fk]
+        begin = _undo['firstlog'][foreign_key]
 
         if is_undo:
-            self.redostack[fk].append((begin, end))
+            self.redostack[foreign_key].append((begin, end))
         else:
-            self.undostack[fk].append((begin, end))
-        self.start_interval(fk)
+            self.undostack[foreign_key].append((begin, end))
+        self.start_interval(foreign_key)
 
     def format_for_insert(self, data):
         """Format a row for insert.
-        
+
         Deletes non-insertable values from the row. Overwrite if more than ID needs
         to be deleted.
         """
@@ -696,11 +692,12 @@ class SQLTableBase:
 
 
 class CatalogueTable(SQLTableBase):
+    """Super class for operations of a catalogue table."""
     def __init__(self, connection, catalogue):
         super().__init__(connection)
         catalogue.create()
         self.catalogue = catalogue
-    
+
     def get_catalogue_table(self):
         """Return the connected catalogue table."""
         return self.catalogue
